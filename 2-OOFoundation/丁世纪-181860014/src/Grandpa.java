fa359bc6995c27cbab3ef722a4ccdda3da7b9f25
dsj <dsj296631741@163.com>
2020-09-18 15:47:40 +0800

    create files

---
+++ 2-OOFoundation/丁世纪-181860014/src/Grandpa.java
@@ -0,0 +1,7 @@
+//指示一个葫芦娃做出动作
+
+
+
+public class Grandpa {
+    
+}
---
+++ 2-OOFoundation/丁世纪-181860014/src/HuluBrother.java
@@ -0,0 +1,35 @@
+//
+
+
+public class HuluBrother {
+   String name;
+   int rank;//  老大rank=0
+   
+
+   HuluBrother()
+   {
+       name="ungiven";
+       rank=-1;
+   }
+   HuluBrother(String s,int i)
+   {
+        name=s;
+        rank=i;
+   }
+
+   void Init(String s,int i)
+   {
+        name=s;
+        rank=i;
+   }
+
+   void Self_intro()
+   {
+       System.out.println("My name is "+name);
+       System.out.println("The "+rank+"th boy");    //nevermind 1th
+   }
+
+ 
+
+
+}
