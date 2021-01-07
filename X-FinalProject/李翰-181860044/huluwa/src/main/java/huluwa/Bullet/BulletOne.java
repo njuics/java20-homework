25fbf6b33d82f847549892127b12eedf8740dbaf
Eternity-AIBN <1554287628@qq.com>
2021-01-07 09:36:24 +0800

    .

--- X-FinalProject/李翰-181860044/huluwa/src/main/java/huluwa/Bullet/BulletOne.java
+++
@@ -1,7 +0,0 @@
-package huluwa.Bullet;
-
-public class BulletOne extends Bullet{
-    BulletOne(){
-        super(5);
-    }
-}
--- X-FinalProject/李翰-181860044/huluwa/src/main/java/huluwa/Creature/Pangolin.java
+++
@@ -1,7 +0,0 @@
-package huluwa.Creature;
-
-public class Pangolin extends Creature{
-    public Pangolin(String name, int fullHP, int defence, String bulletType, int posX, int posY){
-        super(name, fullHP, defence, bulletType, posX, posY, true);
-    }
-}
